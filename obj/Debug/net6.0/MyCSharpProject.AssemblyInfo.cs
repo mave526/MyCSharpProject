123efc8fefc210bcf3409b542154d7dc802ee1cf
mave526 <136264791+mave526@users.noreply.github.com>
2025-03-10 03:23:40 +0800

    Interpolation

--- obj/Debug/net6.0/MyCSharpProject.AssemblyInfo.cs
+++ obj/Debug/net6.0/MyCSharpProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCSharpProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9e3d21d8f433dbc877483c96a04190e3885c53c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ad3017d7b9bb7c32183e175a683285733fa9578")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCSharpProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCSharpProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/Program.cs
+++ src/Program.cs
@@ -8,19 +8,27 @@ namespace HelloWorld
   {
     static void Main(string[] args)
     {
-      string varName;
+      string varName = "John";
       // string input;
       int varAge, varHeight;
-      varName = Console.ReadLine();
-      varAge = 45;
+      varAge = Convert.ToInt32(Console.ReadLine());
+      
       varHeight = 170;
       // varWeight = 75;
-      Console.WriteLine("My name is " + varName + " and my age is " + varAge + varAge); 
+      Console.WriteLine("My name is " + varName + " and my age is " + varAge); 
       // input = Console.ReadLine();
       // Console.WriteLine(input);
 
       Console.Write(varAge + varHeight);
 
-    }
+      bool IsPowerOfTwo(int x)
+        {
+            return (x > 0) && ((x & (x - 1)) == 0);
+        }
+
+      Console.WriteLine("Is the number a power of two?");
+      int input = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine(IsPowerOfTwo(input));
+      }
   }
 }
